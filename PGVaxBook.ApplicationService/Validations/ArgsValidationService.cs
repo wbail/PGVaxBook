66d36589a12eadbdf41b99a7ad73d558772fe4db
wbail <guilhermedanbail@gmail.com>
2022-02-13 20:18:47 +0000

    feat: add validation service

---
+++ PGVaxBook.ApplicationService/Validations/ArgsValidationService.cs
@@ -0,0 +1,17 @@
+﻿namespace PGVaxBook.ApplicationService.Validations;
+
+public class ArgsValidationService : IArgsValidationService
+{
+    public bool IsFilePathExists(string[] args)
+    {
+        return args.Any() &&
+            !string.IsNullOrEmpty(args[0]) &&
+            File.Exists(args[0]);
+    }
+
+    public bool IsSingleArgument(string[] args)
+    {
+        return args.Any() &&
+            args.Length == 1;
+    }
+}
---
+++ PGVaxBook.ApplicationService/Validations/IArgsValidationService.cs
@@ -0,0 +1,7 @@
+﻿namespace PGVaxBook.ApplicationService.Validations;
+
+public interface IArgsValidationService
+{
+    bool IsFilePathExists(string[] args);
+    bool IsSingleArgument(string[] args);
+}

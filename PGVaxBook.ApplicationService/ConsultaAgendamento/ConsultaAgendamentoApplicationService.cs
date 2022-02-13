f60a869507c2d8252fde4b16906a5ca84fe4dbbf
wbail <guilhermedanbail@gmail.com>
2022-02-13 20:18:27 +0000

    feat: receives a list of consultas

--- PGVaxBook.ApplicationService/ConsultaAgendamento/ConsultaAgendamentoApplicationService.cs
+++ PGVaxBook.ApplicationService/ConsultaAgendamento/ConsultaAgendamentoApplicationService.cs
@@ -19,6 +19,31 @@ public class ConsultaAgendamentoApplicationService : IConsultaAgendamentoApplica
         return IsConsultaAgendamentoExists(response.Html);
     }
 
+    public async Task<List<string>> ConsultaAgendamento(List<ConsultaAgendamentoApplicationRequest> consultaAgendamentoRequests, int delayInMilliseconds)
+    {
+        var consultaAgendamentoResponseList = new List<string>();
+
+        foreach (var consultaAgendamentoRequest in consultaAgendamentoRequests)
+        {
+            var response = await _consultaAgendamentoService.ConsultaAgendamento(consultaAgendamentoRequest);
+
+            var isAgendado = IsConsultaAgendamentoExists(response.Html);
+
+            if (isAgendado)
+            {
+                consultaAgendamentoResponseList.Add($"O CPF {consultaAgendamentoRequest.Cpf} esta agendado");
+            }
+            else
+            {
+                consultaAgendamentoResponseList.Add($"O CPF {consultaAgendamentoRequest.Cpf} NAO esta agendado");
+            }
+
+            await Task.Delay(delayInMilliseconds);
+        }
+
+        return consultaAgendamentoResponseList;
+    }
+
     private bool IsConsultaAgendamentoExists(string responseMessage)
     {
         //var exists = responseMessage.Contains("consultado está pendente") || responseMessage.Contains("não foi agendado");

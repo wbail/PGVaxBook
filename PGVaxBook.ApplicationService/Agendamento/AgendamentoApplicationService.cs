ae796cf8d361a7eb1a1bd511c1b65cdb79fa19b6
wbail <guilhermedanbail@gmail.com>
2022-02-13 20:18:16 +0000

    feat: receives a list of agendamentos

--- PGVaxBook.ApplicationService/Agendamento/AgendamentoApplicationService.cs
+++ PGVaxBook.ApplicationService/Agendamento/AgendamentoApplicationService.cs
@@ -26,4 +26,27 @@ public class AgendamentoApplicationService : IAgendamentoApplicationService
 
         return $"Agendamento efetuado para {agendamentoRequest.Nome}";
     }
+
+    public async Task<List<string>> MakeAgendamento(List<AgendamentoApplicationRequest> agendamentoRequests, int delayInMilliseconds)
+    {
+        var agendamentoResponseList = new List<string>();
+
+        foreach (var agendamentoRequest in agendamentoRequests)
+        {
+            var agendamentoResponse = await _agendamentoService.MakeAgendamento(agendamentoRequest);
+
+            if (agendamentoResponse.Erro)
+            {
+                var pattern = @">\W+[a-zA-Z]\W.+";
+                var errorMessage = Regex.Match(agendamentoResponse.Html, pattern).Value;
+                agendamentoResponseList.Add(errorMessage);
+            }
+
+            agendamentoResponseList.Add($"Agendamento efetuado para {agendamentoRequest.Nome}");
+
+            await Task.Delay(delayInMilliseconds);
+        }
+
+        return agendamentoResponseList;
+    }
 }
--- PGVaxBook.ApplicationService/Agendamento/IAgendamentoApplicationService.cs
+++ PGVaxBook.ApplicationService/Agendamento/IAgendamentoApplicationService.cs
@@ -5,4 +5,5 @@ namespace PGVaxBook.ApplicationService.Agendamento;
 public interface IAgendamentoApplicationService
 {
     Task<string> MakeAgendamento(AgendamentoApplicationRequest agendamentoRequest);
+    Task<List<string>> MakeAgendamento(List<AgendamentoApplicationRequest> agendamentoRequests, int delayInMilliseconds);
 }

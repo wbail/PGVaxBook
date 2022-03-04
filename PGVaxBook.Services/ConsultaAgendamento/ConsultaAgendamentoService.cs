68f85960f1a12f6e6d94511493dc5a668e0a14e6
wbail <guilhermedanbail@gmail.com>
2022-03-04 18:20:48 +0000

    refact: remove unnecessary code

--- PGVaxBook.Services/ConsultaAgendamento/ConsultaAgendamentoService.cs
+++ PGVaxBook.Services/ConsultaAgendamento/ConsultaAgendamentoService.cs
@@ -1,12 +1,5 @@
-﻿using Newtonsoft.Json;
-using PGVaxBook.Messages.Requests;
+﻿using PGVaxBook.Messages.Requests;
 using PGVaxBook.Messages.Responses;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace PGVaxBook.Services.ConsultaAgendamento;
 
@@ -22,14 +15,6 @@ public class ConsultaAgendamentoService : IConsultaAgendamentoService
 
     public async Task<ConsultaAgendamentoApplicationResponse> ConsultaAgendamento(ConsultaAgendamentoApplicationRequest consultaAgendamentoRequest)
     {
-        /*
-         <h2 class="text-center"><i>COMPROVANTE DE AGENDAMENTO</i><br /><u>COVID-19</u></h2><br/><br/>
-<div class="alert alert-danger" role="alert">
-O CPF 03731137909 consultado está pendente de distribuição de horário. <br/>Aguarde até as vagas esgotarem ou após as 19h para consultar novamente.
-        </div><br/><i>Comprovante gerado em 09/02/2022 19:06:55</i>
-
-         */
-
         var url = $"{_uri}/app/consulta";
         Uri uri = new Uri(url);
 
@@ -44,8 +29,6 @@ O CPF 03731137909 consultado está pendente de distribuição de horário. <br/>
 
         var responseMessage = await response.Content.ReadAsStringAsync();
 
-        //var consultaAgendamentoResponse = await JsonSerializer.DeserializeAsync<ConsultaAgendamentoApplicationResponse>(responseMessage);
-        //var consultaAgendamentoResponse = JsonConvert.DeserializeObject<ConsultaAgendamentoApplicationResponse>(responseMessage);
         var consultaAgendamentoResponse = new ConsultaAgendamentoApplicationResponse();
         consultaAgendamentoResponse.Html = responseMessage;
         return consultaAgendamentoResponse;

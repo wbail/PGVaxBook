a7cf791c24aa66bcfd83ddcdb57c668b42062ab4
wbail <guilhermedanbail@gmail.com>
2022-03-04 18:16:51 +0000

    feat: add string filter

---
+++ PGVaxBook.ApplicationService/Extensions/StringFilterExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace PGVaxBook.ApplicationService.Extensions
+{
+    public static class StringFilterExtension
+    {
+        public static string Filter(this string str, List<string> stringsToRemove)
+        {
+            foreach (var c in stringsToRemove)
+            {
+                str = str.Replace(c.ToString(), String.Empty);
+            }
+
+            return str;
+        }
+    }
+}
